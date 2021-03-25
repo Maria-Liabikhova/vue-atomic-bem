b268eb35b07551a14f88f31edf0294c8b8d501cf
unknown <mashagi@yandex.ru>
2021-03-25 17:00:08 +0300

    footer finished

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,42 +1,51 @@
 <template>
-  <div class="footer pt-322 bg--green-dark">
+  <footer class="footer bg--green-dark">
+    <div class="w-90 d-flex bg--whites h-25 py-70 footer__banner">
+      <div class="w-15"></div>
+      <div>
+        <h2 class="text--green text--bold text--60 lh--74 mb-50">Let’s make something<br>amazing together.</h2>
+        <a href="" class="link  text--uppercase text--gray">
+          Let’s get started
+        </a>
+      </div>
+    </div>
     <div class="container">
-      <div class="d-flex w-100 pt-120">
-        <div class=" w-20 mb-55">
-          <h6 class="lh-40 text--whites mb-8 text--green-light">Phone</h6>
+      <div class="d-flex w-100">
+        <div class="w-20 mb-55">
+          <h3 class="lh-40 text--whites mb-8 text--green-light">Phone</h3>
           <a href='tel:+3250312832' class=" link w-20 lh-40 text--bold text--whites footer__no-wrapp">+32 50 31 28 32</a>
         </div>
         <div class="w-20 mb-55">
-          <h6 class="lh-40 text--whites mb-8 text--green-light">Address</h6>
+          <h3 class="lh-40 text--whites mb-8 text--green-light">Address</h3>
           <address class="lh-40  text--bold text--whites">491 Merlin Crest Suite 963</address>
         </div>
       </div>
       <div class="d-flex py-60 w-100 footer__border">
-        <div class="w-20 d-flex flex-column">
-          <h6 class="lh-38 text--uppercase text--16 text--whites mb-20">Services</h6>
-          <a href="#" class="link mb-15 text--green-light lh-40 ">Brand Strategy</a>
-          <a href="#" class="link mb-15 text--green-light lh-40">Product Design</a>
-          <a href="#" class="link mb-15 text--green-light lh-40">Content Strategy</a>
-          <a href="#" class="link mb-15 text--green-light lh-40">Brand Strategy</a>
-          <a href="#" class="link mb-15 text--green-light lh-40">Development</a>
+        <div class="w-20 d-block flex-column">
+          <h3 class="lh-38 text--uppercase text--16 text--whites mb-20">Services</h3>
+          <a href="#" class="link d-block mb-15 text--green-light lh-40 ">Brand Strategy</a>
+          <a href="#" class="link d-block mb-15 text--green-light lh-40">Product Design</a>
+          <a href="#" class="link d-block mb-15 text--green-light lh-40">Content Strategy</a>
+          <a href="#" class="link d-block mb-15 text--green-light lh-40">Brand Strategy</a>
+          <a href="#" class="link d-block mb-15 text--green-light lh-40">Development</a>
         </div>
-        <div class="w-25 d-flex flex-column">
-          <h6 class="lh-38 text--uppercase text--16 text--whites mb-20 footer__no-wrapp">Help And Advice</h6>
-          <a href="#" class="link mb-15 text--green-light lh-40">How it works</a>
-          <a href="#" class="link mb-15 text--green-light lh-40">Contact Support</a>
-          <a href="#" class="link mb-15 text--green-light lh-40">Privacy Policy</a>
-          <a href="#" class="link mb-15 text--green-light lh-40 ">FAQ</a>
+        <div class="w-25 d-block flex-column">
+          <h3 class="lh-38 text--uppercase text--16 text--whites mb-20 footer__no-wrapp">Help And Advice</h3>
+          <a href="#" class="link d-block mb-15 text--green-light lh-40">How it works</a>
+          <a href="#" class="link d-block mb-15 text--green-light lh-40">Contact Support</a>
+          <a href="#" class="link d-block mb-15 text--green-light lh-40">Privacy Policy</a>
+          <a href="#" class="link d-block mb-15 text--green-light lh-40 ">FAQ</a>
         </div>
-        <div class="w-20 d-flex flex-column footer__border--y">
-          <h6 class="lh-38 text--uppercase text--16 text--whites mb-20">Company</h6>
-          <a href="#" class="link mb-15 text--green-light lh-40">About</a>
-          <a href="#" class="link mb-15 text--green-light lh-40">Blog</a>
-          <a href="#" class="link mb-15 text--green-light lh-40">Contact</a>
-          <a href="#" class="link mb-15 text--green-light lh-40 ">Jobs</a>
+        <div class="w-20 d-block flex-column footer__border--y">
+          <h3 class="lh-38 text--uppercase text--16 text--whites mb-20">Company</h3>
+          <a href="#" class="link d-block mb-15 text--green-light lh-40">About</a>
+          <a href="#" class="link d-block mb-15 text--green-light lh-40">Blog</a>
+          <a href="#" class="link d-block mb-15 text--green-light lh-40">Contact</a>
+          <a href="#" class="link d-block mb-15 text--green-light lh-40 ">Jobs</a>
         </div>
-        <div class="w-35 d-flex flex-column">
+        <div class="w-35">
           <div class="px-30">
-          <p class="lh-38 text--uppercase text--16 text--whites mb-20">Get in Touch</p>
+          <h3 class="lh-38 text--uppercase text--16 text--whites mb-20">Get in Touch</h3>
           <p class="lh-40 text--whites text--green-light mb-20">Feel free to get in touch with us vai email</p>
           <a href = "mailto: info.webovio@gmail.com" class="link text--24 text--bold lh-38 ">info.webovio@gmail.com</a>
           <div class="w-70 pt-25">
@@ -59,10 +68,8 @@
             <span class="text--whites text--24 lh--25">webovio<span class="text--yellow text--36">.</span></span>
           </a>
         </div>
-        <div class="d-flex">
-          <div class="lh-40 text--whites text--green-light">© 2020@webovio. All Rights Reserved.</div>
-        </div>
+        <div class="lh-40 text--whites text--green-light"><p>© 2020@webovio. All Rights Reserved.</p></div>
       </div>
     </div>
-  </div>
+  </footer>
 </template>
