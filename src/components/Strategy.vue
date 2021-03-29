e4bd77cebc3af8b80c4242924b0bb8c7f79d0507
unknown <mashagi@yandex.ru>
2021-03-29 13:41:14 +0300

    strategy section adapted

--- src/components/Strategy.vue
+++ src/components/Strategy.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="main bg--white mb-90">
-    <div class="container">
-      <div class="w-40 d-inline-block pb-30">
-        <p class="text--dark text--14 mb-30 pt-auto">
+  <div class="main bg--white mb-90 main__strategy">
+    <div class="container d-flex d-sm-block">
+      <div class="w-45 d-inline-block">
+        <p class="text--dark pt-30 pt-sm-0 text--10 text--sm-14 mb-30 pt-auto">
           What we do for you
         </p>
-        <h2 class="text--green text--36 text--bold mb-25 lh--46">
+        <h2 class="text--green text--22 text--sm-30 text--lg-36 text--bold mb-25 lh--46">
           Strategy. Design<br>Content. Technology<br>Development 
         </h2>
         <p class="text--gray text--14 mb-30">
@@ -14,13 +14,13 @@
         <a href="#" class="link link__gray text--gray-l">More know About us</a>
       </div>
       <div class="w-10 d-inline-block"></div>
-      <div class="w-40 d-inline-block px-30 py-30 bg--whites radius--4 main__shadow">
-        <p class="text--24 lh-40 mb-50 pt-225"><i>With More than 10 Years of Knowledge and Expertise we Design and Code Websites and Apps, We Build Brands and Help Them Succeed</i></p>
-        <div class="d-flex">
+      <div class="w-45 d-inline-block px-10 py-10 px-sm-30 py-sm-30 bg--whites radius--4 main__shadow h-100 ">
+        <p class="text--12 text--sm-16 text--lg-24 lh-40 mb-50 pt-124 pt-md-181 pt-lg-225"><i>With More than 10 Years of Knowledge and Expertise we Design and Code Websites and Apps, We Build Brands and Help Them Succeed</i></p>
+        <div class="d-md-flex">
           <div class="main__avatar main__avatar--strategy"></div>
           <div>
-            <p class="mb-0 text--22 text--bold">Genevieve Rodriquez</p>
-            <p class="lh-21 mb-0">Founder & CEO, Webovio</p>
+            <p class="mb-0 text--12 text--md-16 text--lg-22 text--bold">Genevieve Rodriquez</p>
+            <p class="lh-21 text--12 text--md-14 text--lg-16 mb-0">Founder & CEO, Webovio</p>
           </div>
         </div>
       </div>
